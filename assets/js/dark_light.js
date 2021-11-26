40b58f1afb6ec5b7c050b1595907223c307a62df
Abhishek Kumar <abhishekpawan297@gmail.com>
2021-11-27 02:51:45 +0530

    uploading

---
+++ assets/js/dark_light.js
@@ -0,0 +1,43 @@
+const btn = document.querySelector(".btn-toggle");
+const dl_icon=document.querySelector("#dl_icon")
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// const currentTheme = localStorage.getItem("theme");
+const currentTheme = "light";
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+  dl_icon.classList.toggle("bxs-sun");
+  
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+  dl_icon.classList.toggle("bxs-moon");
+
+}
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
+
+
+btn.addEventListener("click", function(){
+  if(dl_icon.classList.contains("bxs-moon")){
+  dl_icon.classList.remove("bxs-moon");
+  dl_icon.classList.add("bxs-sun");
+  }
+
+  else{
+    dl_icon.classList.remove("bxs-sun");
+    dl_icon.classList.add("bxs-moon");
+  }
+})
---
+++ assets/js/scroll.js
@@ -0,0 +1,33 @@
+(function($) {
+
+	$(".container").on('scroll', function() {
+    	$val = $(this).scrollLeft();
+
+    	if($(this).scrollLeft() + $(this).innerWidth()>=$(this)[0].scrollWidth){
+          $(".nav-next").hide();
+        } else {
+    		$(".nav-next").show();
+    	}
+
+    	if($val == 0){
+    		$(".nav-prev").hide();
+    		$(".heading").show();
+
+    	} else {
+    		$(".nav-prev").show();
+    		$(".heading").hide();
+
+    	}
+  	});
+      const width=$(".container").innerWidth();
+      
+	$(".nav-next").on("click", function(){
+		$(".container").animate( { scrollLeft: `+=${width}` }, 200);
+	});
+	$(".nav-prev").on("click", function(){
+		$(".container").animate( { scrollLeft: `-=${width}` }, 200);
+	});
+
+	
+
+})(jQuery);
